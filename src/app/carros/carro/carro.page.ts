292665bb8c2e6db51ccf06772be41dff2f3bc1cf
Fernando Camacho <fernando.camacho@peopleart.co>
2025-06-11 21:04:41 -0500

    carro

--- src/app/carros/carro/carro.page.ts
+++ src/app/carros/carro/carro.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-carro',
   templateUrl: './carro.page.html',
   styleUrls: ['./carro.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class CarroPage implements OnInit {
 
